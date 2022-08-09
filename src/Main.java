2f9b8131760307ca43b3b2eb8065df3b0b2a0514
V-keerththanan <keertht-se19040@stu.kln.ac.lk>
2022-08-09 11:18:44 +0530

    Add just initial page include option like log in, register,Exit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
-class welcome{
-    welcome(){
-        System.out.println("Welcome to mysql");
-    }
-}
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        welcome obj=new welcome();
+    static Scanner scan = new Scanner(System.in);
+    public static int doLoginPage(){
+        int select;
+        System.out.println("\n\t\t=======Welcome to CLI Facebook======\t");
+        System.out.println("\t\t\t1.Log in\t");
+        System.out.println("\t\t\t2.Register\t");
+        System.out.println("\t\t\t3.Exit\t");
+        System.out.println("Select option 1,2,3 to do your option?");
+        select=scan.nextInt();
+        return select;
     }
+
+    public static void main(String[] args){
+        int option= doLoginPage();
+    }
+
+
 }
